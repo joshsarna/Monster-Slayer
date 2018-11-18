d5ce0285139f05242c4843933f588636ac6269ed
joshsarna <joshsarna@gmail.com>
2018-11-17 22:26:55 -0800

    add functionality to special-attack button

--- app.js
+++ app.js
@@ -17,6 +17,14 @@ new Vue({
       this.moves.push('PLAYER HITS MONSTER FOR ' + playerAttack);
       this.playerHealth -= monsterAttack;
       this.moves.push('MONSTER HITS PLAYER FOR ' + monsterAttack);
+    },
+    specialAttack: function() {
+      var playerAttack = Math.floor(10 + Math.random() * 8);
+      var monsterAttack = Math.floor(5 + Math.random() * 8);
+      this.monsterHealth -= playerAttack;
+      this.moves.push('PLAYER HITS MONSTER FOR ' + playerAttack);
+      this.playerHealth -= monsterAttack;
+      this.moves.push('MONSTER HITS PLAYER FOR ' + monsterAttack);
     }
   }
 });
