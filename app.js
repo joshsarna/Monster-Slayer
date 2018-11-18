d971a395ef9131844e84c2f545febbd51d162f0a
joshsarna <joshsarna@gmail.com>
2018-11-17 22:25:03 -0800

    add functionality to attack button

--- app.js
+++ app.js
@@ -5,6 +5,18 @@ new Vue({
   data: {
     gameStarted: false,
     playerHealth: 100,
-    monsterHealth: 100
+    monsterHealth: 100,
+    moves: []
+  },
+
+  methods: {
+    attack: function() {
+      var playerAttack = Math.floor(5 + Math.random() * 8);
+      var monsterAttack = Math.floor(5 + Math.random() * 8);
+      this.monsterHealth -= playerAttack;
+      this.moves.push('PLAYER HITS MONSTER FOR ' + playerAttack);
+      this.playerHealth -= monsterAttack;
+      this.moves.push('MONSTER HITS PLAYER FOR ' + monsterAttack);
+    }
   }
 });
